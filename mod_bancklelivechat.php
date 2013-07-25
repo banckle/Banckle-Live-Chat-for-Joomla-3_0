fd4262d33e44a771656d009f70e76a648709ae96
Assad Mahmood Qazi <assadvirgo@gmail.com>
2013-07-25 13:02:01 +0500

    Initial Commit

---
+++ mod_bancklelivechat.php
@@ -0,0 +1,14 @@
+<?php
+/*------------------------------------------------------------------------
+# mod_bancklelivechat - Banckle Live Chat for Joomla
+# ------------------------------------------------------------------------
+# author    Imran Anwar <imran.anwar@banckle.com>
+# copyright Copyright (C) 2012 banckle.com. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://banckle.com
+# Technical Support:  Docs - http://banckle.com/wiki/display/livechat/integration-with-joomla.html
+-------------------------------------------------------------------------*/
+// no direct access
+defined( "_JEXEC" ) or die;
+
+require(JModuleHelper::getLayoutPath('mod_bancklelivechat'));
---
+++ tmpl/default.php
@@ -0,0 +1,18 @@
+<?php
+/*------------------------------------------------------------------------
+# mod_bancklelivechat - Banckle Live Chat for Joomla
+# ------------------------------------------------------------------------
+# author    Imran Anwar <imran.anwar@banckle.com>
+# copyright Copyright (C) 2012 banckle.com. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://banckle.com
+# Technical Support:  Docs - http://banckle.com/wiki/display/livechat/integration-with-joomla.html
+-------------------------------------------------------------------------*/
+// no direct access
+defined('_JEXEC') or die('Restricted access'); ?>
+
+<div id=banckleLiveChatButton>
+<?php
+    echo $params->get("banckleLCWidget");
+?>
+</div>
